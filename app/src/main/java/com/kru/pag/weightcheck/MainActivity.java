51a8f2f675f6c28c0358321e91abe4ad8e424cdb
kittikon <pagsky_2499@hotmail.com>
2016-02-02 20:35:30 -0800

    Create SQLLite Suscessfull

--- app/src/main/java/com/kru/pag/weightcheck/MainActivity.java
+++ app/src/main/java/com/kru/pag/weightcheck/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //Explicit
+    private myManage objMyManage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        //Request Database
+        objMyManage = new myManage(this);
+    }//Main Constacter
+}// Main Class
