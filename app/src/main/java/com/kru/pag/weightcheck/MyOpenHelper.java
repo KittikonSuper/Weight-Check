067a8f9da1a6c00e552be8b700b908dcc8c760a9
kittikon <pagsky_2499@hotmail.com>
2016-02-02 20:02:39 -0800

    Create MyOpenHelper SuceesFull

--- app/src/main/java/com/kru/pag/weightcheck/MyOpenHelper.java
+++ app/src/main/java/com/kru/pag/weightcheck/MyOpenHelper.java
@@ -1,9 +1,37 @@
 package com.kru.pag.weightcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by pag on 2/2/2559.
  */
-public class MyOpenHelper {
-    public MyOpenHelper() {
+public class MyOpenHelper extends SQLiteOpenHelper{
+
+
+    //Explicit
+    public static final String database_name = "Weight.db";//ประกาศค่าคงที่ ที่ไม่สามารถแก้ไขได้
+    private static final int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key, " +
+            "Date text," +
+            "Weight text);";
+
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
     }// constructor
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 }// main class นี้คือคลาส
