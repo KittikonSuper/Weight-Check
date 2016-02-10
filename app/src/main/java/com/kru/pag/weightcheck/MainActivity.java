3a685b325888557a3a8bfa0827e050d78ca8d9a0
kittikon <pagsky_2499@hotmail.com>
2016-02-09 19:14:38 -0800

    check regit finish

--- app/src/main/java/com/kru/pag/weightcheck/MainActivity.java
+++ app/src/main/java/com/kru/pag/weightcheck/MainActivity.java
@@ -1,5 +1,8 @@
 package com.kru.pag.weightcheck;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Request Database
         objMyManage = new myManage(this);
+        //chack Regitter
+        checkRegiter();
     }//Main Constacter
+
+    private void checkRegiter() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name, MODE_PRIVATE, null);
+        Cursor objCursor = objSqLiteDatabase.rawQuery("SELECT * FROM userTABLE", null);
+
+        if (objCursor.getCount() == 0) {
+            Intent objIntent = new Intent(MainActivity.this, RegitterActivity.class);
+            startActivity(objIntent);
+
+        }//if
+        objCursor.close();
+    }//checkRegiter
 }// Main Class
