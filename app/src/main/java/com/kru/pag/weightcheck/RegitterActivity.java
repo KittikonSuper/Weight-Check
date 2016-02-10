18dc3af236636af1f04d39f8838d3249642cfa75
kittikon <pagsky_2499@hotmail.com>
2016-02-09 20:39:07 -0800

    desige Dl regit แล้ว

--- app/src/main/java/com/kru/pag/weightcheck/RegitterActivity.java
+++ app/src/main/java/com/kru/pag/weightcheck/RegitterActivity.java
@@ -2,12 +2,49 @@ package com.kru.pag.weightcheck;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegitterActivity extends AppCompatActivity {
+    //Explicit
+    private EditText userEdittext,PasswordEdittext, NameEdittext;
+    private String userString,passwordString, nameString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regitter);
+
+        //Bind Widget
+        BindWidget();
+
+
+    }//Main Method
+
+    private void BindWidget() {
+        userEdittext = (EditText) findViewById(R.id.editText);
+        PasswordEdittext = (EditText) findViewById(R.id.editText2);
+        NameEdittext = (EditText) findViewById(R.id.editText3);
+
     }
-}
+
+    public void clickRegit(View view) {
+        userString = userEdittext.getText().toString().trim();
+        passwordString = PasswordEdittext.getText().toString().trim();
+        nameString = NameEdittext.getText().toString().trim();
+        if (userString.equals("")|| passwordString.equals("") || nameString.equals("")) {
+            //Have Space
+            Toast.makeText(RegitterActivity.this,getResources().getString(R.string.have_space),
+                    Toast.LENGTH_LONG).show();
+        } else {
+            // No Space
+            myManage objmyManage = new myManage(this);
+            objmyManage.addUserTable(userString, passwordString, nameString);
+            Toast.makeText(RegitterActivity. this, getResources().getString(R.string.success_Regit),
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        } //if
+
+    }//clickRegit
+}//Main Class
--- app/src/main/java/com/kru/pag/weightcheck/myManage.java
+++ app/src/main/java/com/kru/pag/weightcheck/myManage.java
@@ -1,5 +1,6 @@
 package com.kru.pag.weightcheck;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -10,6 +11,14 @@ public class myManage {
     //Explicit
     private MyOpenHelper objmyOpenHelper;
     private SQLiteDatabase writeSqliteDatabase, readSqLiteDatabase;
+    public static final String table_user = "user TABLE";
+    public static final String table_weight = "weightTABLE ";
+    public static final String Column_user = "_id";
+    public static final String Column_Password = "_Password";
+    public static final String Column_name = "_Name";
+    public static final String Column_data = "_Data";
+    public static final String Column_weight = "_weight";
+
 
     public myManage(Context context) {
         //Creat & Connected
@@ -19,5 +28,15 @@ public class myManage {
 
 
     }//Constacter
+    //Add New Value to userTABLE
+    public long addUserTable(String strUser, String strPassword, String strName) {
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(Column_user, strUser);
+        objContentValues.put(Column_Password, strPassword);
+        objContentValues.put(Column_name, strName);
+
+
+        return writeSqliteDatabase.insert(table_user, null,objContentValues);
+    }
 
 }//Main Class
